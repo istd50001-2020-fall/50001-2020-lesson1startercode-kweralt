b859f468c2d57638ca871e9a662beb8b8d3b0e9a
kweralt <51038342+kweralt@users.noreply.github.com>
2020-11-02 16:19:40 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -7,8 +7,13 @@ package com.example.myfirstapp;
 // ***Sep 2020
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.Random;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 //TODO 1.1 Put in some images in the drawables folder
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     //TODO 1.2 Instance variables are declared for you, please import the libraries
     ArrayList<Integer> images;
     Button charaButton;
+    Button charbButton;
     ImageView charaImage;
     int count = 0;
 
@@ -28,13 +34,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //TODO 1.3 Instantiate An ArrayList object
+        ArrayList<Integer> images = new ArrayList<Integer>();
         //TODO 1.4 Add the image IDs to the ArrayList
+        images.add(R.drawable.ashketchum);
+        images.add(R.drawable.bartsimpson);
+        images.add(R.drawable.pikachu);
+        images.add(R.drawable.nickwilde);
+        images.add(R.drawable.edogawaconan);
+        images.add(R.drawable.judyhopps);
+        images.add(R.drawable.nemo);
+        images.add(R.drawable.snorlax);
+        images.add(R.drawable.tomandjerry);
+        images.add(R.drawable.yoda);
         //TODO 1.5 Get references to the charaButton and charaImage widgets using findViewById
+        charaButton = findViewById(R.id.charaButton);
+        charaImage = findViewById(R.id.charaImage);
         //TODO 1.6 For charaButton, invoke the setOnClickListener method
         //TODO 1.7 Create an anonymous class which implements View.OnClickListener interface
         //TODO 1.8 Within onClick, write code to randomly select an image ID from the ArrayList and display it in the ImageView
+        charaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int id = images.get(random.nextInt(images.size()));
+                charaImage.setImageResource(id);
+            }
+        });
         //TODO 1.9 [On your own] Create another button, which when clicked, will cause one image to always be displayed
-
+        charbButton = findViewById(R.id.charbButton);
+        charbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                charaImage.setImageResource(images.get(2));
+            }
+        });
 
     }
 }
